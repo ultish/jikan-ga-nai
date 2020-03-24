052dca08f8f52b34a6706c39428b3f530a6c36fe
Jimmy <ultish@gmail.com>
2020-03-24 14:16:37 +1100

    separted concerns

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import schema from './schema';
 import resolvers from './resolvers';
-import models from './models';
+import models, { sequelize } from './models';
 
 import cors from 'cors';
 import 'dotenv/config';
@@ -22,6 +22,9 @@ const server = new ApolloServer({
   },
 });
 server.applyMiddleware({ app, path: '/graphql' });
-app.listen({ port: 8000 }, () => {
-  console.log('Apollo Server on http://localhost:8000/graphql');
+
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+  });
 });
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,31 +1,23 @@
-let users = {
-  1: {
-    // field names here must match field names defined
-    // in the schema. case sensitive
-    id: '1',
-    username: 'Robin Wieruch',
-    messageIds: [1],
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-    messageIds: [2],
-  },
-};
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
+  }
+);
+
+const models = {
+  User: sequelize.import('./user'),
+  Message: sequelize.import('./message'),
 };
 
-export default {
-  users,
-  messages,
-};
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+export { sequelize };
+export default models;

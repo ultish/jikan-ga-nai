ef36c98ba697de6a666801ed27841719979f5152
Jimmy <ultish@gmail.com>
2020-03-24 14:49:21 +1100

    using postgres with sequelize

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,68 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+
+// must import this before any uses of process.ENV
+import 'dotenv/config';
+
 import schema from './schema';
 import resolvers from './resolvers';
 import models, { sequelize } from './models';
 
 import cors from 'cors';
-import 'dotenv/config';
 
 console.log(process.env.MY_SECRET);
 
 const app = express();
-
 app.use(cors());
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
+  context: async () => ({
     models,
-    me: models.users[1],
-  },
+    me: await models.User.findByLogin('rwieruch'),
+  }),
 });
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = false;
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
   app.listen({ port: 8000 }, () => {
     console.log('Apollo Server on http://localhost:8000/graphql');
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release...',
+        },
+        {
+          text: 'Published a complete...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+};
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,7 @@
 import Sequelize from 'sequelize';
 
+console.log(process.env.DATABASE);
+
 const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.DATABASE_USER,
